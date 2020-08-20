6205ceee028cb2216eefbc60deb7766b446caaa9
Stud Horse <lalalatim@yahoo.com.tw>
2020-08-20 10:30:50 +0800

    added new root page

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => res.send('it is working now'));
 app.post('/signin', (req, res) => signin.handleSignIn(req, res, db, bcrypt));
 app.post('/register', ( req, res ) => register.handleRegister(req, res, db, bcrypt));
 app.get('/profile/:id', (req, res) => profile.handleProfile((req, res, db)));
