6e6f539adef264d1b5ee79712b535ae86517f78c
yeatin <lalalatim@yahoo.com.tw>
2023-07-26 15:11:09 +0800

    updated cron test

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors());
 
 //scheduled call to DB to remain active
-cron.schedule("5 14 * * Wednesday,Sunday", () => {
+cron.schedule("* * * * Wednesday,Sunday", () => {
     db
     .from('login')
     .select()
