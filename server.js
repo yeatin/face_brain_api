5f25fa9657fc2f21c0b6d6590c6f2e5bccbc9946
yeatin <lalalatim@yahoo.com.tw>
2023-07-27 16:10:30 +0800

    this should fixed the cron syntax once for all

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors());
 
 //scheduled call to DB to remain active
-cron.schedule("0 16 * * THU,MON", () => {
+cron.schedule("0 0 0 ? * THU,MON", () => {
     db
     .from('login')
     .select()
