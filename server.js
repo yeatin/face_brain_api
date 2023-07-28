6bf1d3d48c381a3b698dcde3c2960ae3eaae40a6
yeatin <lalalatim@yahoo.com.tw>
2023-07-28 10:36:29 +0800

    booted supabase and updated cron

--- server.js
+++ server.js
@@ -27,17 +27,18 @@ const db = knex({
 //supabase api
 const db = createClient(process.env.DATABASE_URL, process.env.DATABASE_KEY);
 
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 //scheduled call to DB to remain active
-cron.schedule("0 59 23 * * THU,SUN", () => {
+cron.schedule("0 42 10 * * FRI,MON", () => {
     db
     .from('login')
     .select()
-    .then(user => console.log("Doing cron", new Date()))
-    .catch(err => res.status(400).json('Cron not working'))
+    .then(user => console.log("cron working", new Date()))
+    .catch(err => res.status(400).json('Cron not working', err))
 }, {timezone: "Asia/Taipei"})
 
 app.get('/', (req, res) => res.status(200).send('it is working now'));
